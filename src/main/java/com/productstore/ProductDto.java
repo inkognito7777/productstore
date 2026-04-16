8166a1f5e23b2690f3f4984ab99fe659ba19f4a4
mac <test@mail.ru>
2026-04-16 20:40:04 +0300

    add DTO

---
+++ src/main/java/com/productstore/ProductDto.java
@@ -0,0 +1,15 @@
+package com.productstore;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+}
